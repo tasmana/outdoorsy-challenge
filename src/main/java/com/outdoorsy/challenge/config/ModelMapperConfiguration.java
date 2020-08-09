0c865b7b1c8e441b2fd6001c6c8d98e870c4e605
Hristo Angelov <hristo.angelov@azdio.com>
2020-08-09 10:47:07 +0300

    configured model mapper

--- src/main/java/com/outdoorsy/challenge/config/ModelMapperConfiguration.java
+++ src/main/java/com/outdoorsy/challenge/config/ModelMapperConfiguration.java
@@ -1,6 +1,8 @@
 package com.outdoorsy.challenge.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NamingConventions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,14 @@ public class ModelMapperConfiguration {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.getConfiguration()
+        .setSourceNamingConvention(NamingConventions.NONE)
+        .setSourceNameTransformer((name, nameableType) -> name)
+        .setDestinationNamingConvention(NamingConventions.NONE)
+        .setDestinationNameTransformer((name, nameableType) -> name)
+        .setMatchingStrategy(MatchingStrategies.STRICT);
+
+    return modelMapper;
   }
 }
