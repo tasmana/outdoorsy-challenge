eb7123c04c35163fe733fb3781fef220eaca06b4
Hristo Angelov <hristo.angelov@azdio.com>
2020-08-09 20:17:29 +0300

    configured sentry

---
+++ src/main/java/com/outdoorsy/challenge/config/SentryConfiguration.java
@@ -0,0 +1,23 @@
+package com.outdoorsy.challenge.config;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import io.sentry.spring.SentryExceptionResolver;
+import io.sentry.spring.SentryServletContextInitializer;
+
+@Configuration
+public class SentryConfiguration {
+
+  @Bean
+  public HandlerExceptionResolver sentryExceptionResolver() {
+    return new SentryExceptionResolver();
+  }
+
+  @Bean
+  public ServletContextInitializer sentryServletContextInitializer() {
+    return new SentryServletContextInitializer();
+  }
+}
